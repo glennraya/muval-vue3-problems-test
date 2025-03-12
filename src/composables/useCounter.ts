9dcbde0b118b8d4efbe7119dee8fc94b0cc1f454
Glenn Raya <rayaglenn@gmail.com>
2025-03-13 07:55:46 +0800

    fix: initial_value not incrementing

--- src/composables/useCounter.ts
+++ src/composables/useCounter.ts
@@ -1,11 +1,23 @@
-import { reactive } from 'vue'
-import type { MaybeRef } from 'vue'
+// import { reactive } from 'vue'
+// import type { MaybeRef } from 'vue'
 
-export function useCounter(initial_value: MaybeRef<number> = 0) {
-  const data = reactive({
-    count: initial_value,
-  })
-  const increment = () => data.count++
+// export function useCounter(initial_value: MaybeRef<number> = 0) {
+//   const data = reactive({
+//     count: initial_value,
+//   })
+//   const increment = () => data.count++
 
-  return { ...data, increment }
+//   return { ...data, increment }
+// }
+
+// EXPLANATION: I think in a simple value like a initial_number,
+// 'reactive' is not needed, 'ref' will do and is much simpler.
+import { ref } from 'vue'
+
+export function useCounter(initial_value = 0) {
+    const count = ref(initial_value)
+
+    const increment = () => count.value++
+
+    return { count, increment }
 }
